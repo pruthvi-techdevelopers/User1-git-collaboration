c7fc118aeda5440efab8927d695e0f44cbd6a374
pruthviraja786 <pruthviraja.datascience@gmail.com>
2021-08-30 19:32:34 +0530

    Added 5th line from User2

--- program2.py
+++ program2.py
@@ -6,3 +6,5 @@ print("Added 2nd line")
 print("Added 3rd line")
 #-------------------------------------
 print("Added 4th line from User2 Local system")
+#-------------------------------------
+print("Added 5th line from User2 Local system")
